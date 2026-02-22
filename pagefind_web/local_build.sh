67d91afd611c2f2a3b4caf1e4b93ab2a2205e77d
Mike Fiedler <miketheman@gmail.com>
2026-02-22 05:13:19 -0500

    chore(dev): reduce warnings during builds (#1026)
    * chore(dev): emit markers for language builds
    
    When building locally, it looks like the build is operating in a loop,
    when all it's doing is building each language.
    
    DRY up the language code to a variable for reuse, and emit a
    semi-progress bar so the compiler sees the output.
    
    Signed-off-by: Mike Fiedler <miketheman@gmail.com>
    
    * chore(dev): add values to resolve build warnings
    
    No need to pollute the output with warnings that can be easily resolved.
    
    Signed-off-by: Mike Fiedler <miketheman@gmail.com>
    
    ---------
    
    Signed-off-by: Mike Fiedler <miketheman@gmail.com>

--- pagefind_web/local_build.sh
+++ pagefind_web/local_build.sh
@@ -17,6 +17,7 @@ fi
 rm ../pagefind/vendor/wasm/* || true
 mkdir -p ../pagefind/vendor/wasm
 
+echo "==> Building generic WASM"
 # Build the generic wasm file
 if [ "$1" = "debug" ]; then
     wasm-pack build --dev -t no-modules
@@ -32,17 +33,24 @@ gzip --best ../pagefind/vendor/wasm/pagefind_web_bg.unknown.$WASM_VERSION.wasm
 
 # Build the language-specific wasm files,
 # naively grabbing all features from this crate's Cargo.toml
-grep -e pagefind_stem/ Cargo.toml | while read line ; do
+TOTAL_LANGS=$(grep -c -e pagefind_stem/ Cargo.toml)
+CURRENT_LANG=0
+
+while read line ; do
+    CURRENT_LANG=$((CURRENT_LANG + 1))
+    LANG_CODE=${line:0:2}
+    echo "==> Building language $CURRENT_LANG/$TOTAL_LANGS: $LANG_CODE"
+
     if [ "$1" = "debug" ]; then
-        wasm-pack build --dev -t no-modules --features ${line:0:2}
+        wasm-pack build --dev -t no-modules --features $LANG_CODE
     else
-        RUSTUP_TOOLCHAIN=nightly RUSTFLAGS="-Z unstable-options -C panic=immediate-abort" wasm-pack build --release -t no-modules --features ${line:0:2} --manifest-path ./Cargo.toml -Z build-std=panic_abort,std
+        RUSTUP_TOOLCHAIN=nightly RUSTFLAGS="-Z unstable-options -C panic=immediate-abort" wasm-pack build --release -t no-modules --features $LANG_CODE --manifest-path ./Cargo.toml -Z build-std=panic_abort,std
     fi
 
     # Append pagefind_dcd to the decompressed wasm as a magic word read by the frontend
-    printf 'pagefind_dcd' > ../pagefind/vendor/wasm/pagefind_web_bg.${line:0:2}.$WASM_VERSION.wasm
-    cat pkg/pagefind_web_bg.wasm >> ../pagefind/vendor/wasm/pagefind_web_bg.${line:0:2}.$WASM_VERSION.wasm
-    gzip --best ../pagefind/vendor/wasm/pagefind_web_bg.${line:0:2}.$WASM_VERSION.wasm
-done
+    printf 'pagefind_dcd' > ../pagefind/vendor/wasm/pagefind_web_bg.$LANG_CODE.$WASM_VERSION.wasm
+    cat pkg/pagefind_web_bg.wasm >> ../pagefind/vendor/wasm/pagefind_web_bg.$LANG_CODE.$WASM_VERSION.wasm
+    gzip --best ../pagefind/vendor/wasm/pagefind_web_bg.$LANG_CODE.$WASM_VERSION.wasm
+done < <(grep -e pagefind_stem/ Cargo.toml)
 
 ls -lh ../pagefind/vendor/wasm/
